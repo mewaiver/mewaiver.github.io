db7d56440f6b4f96b005c395f06272efb41a546d
tiago <tiagotg.ribeiro@gmail.com>
2024-11-26 11:13:58 -0300

    fixing await params

--- app/categories/[category]/page.js
+++ app/categories/[category]/page.js
@@ -10,7 +10,7 @@ export async function generateStaticParams() {
 
 export default async function CategoryPage({ params }) {
 
-  const { category } = params;
+  const { category } = await params;
 
   const allPosts = await getAllPosts();
   
