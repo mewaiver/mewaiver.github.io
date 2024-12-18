229604d6584090756c25a2c107d81ec3ea8708b0
tiago <tiagotg.ribeiro@gmail.com>
2024-12-18 16:54:11 -0300

    adding authors picture

--- app/components/Post.js
+++ app/components/Post.js
@@ -7,6 +7,7 @@ import python from 'highlight.js/lib/languages/python';
 import java from 'highlight.js/lib/languages/java'
 import 'highlight.js/styles/github-dark.css';
 import './Post.css'
+import { Image } from 'react-bootstrap';
 hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('python', python);
 hljs.registerLanguage('bash', bash);
@@ -23,12 +24,17 @@ const Post = ({ title, date, content, category, author }) => {
         });
     }, [])
 
+    const formattedAuthorName = author.replace(/\s+/g, '_');
+
     return (
         <>
             <div className="text-center">
                 <h1 className="display-4">{title}</h1>
                 <div className="d-flex justify-content-start">
-                    <p className="text-muted mb-0 me-2"> by {author} - {date}</p>
+                    <Image src={`/authors/${formattedAuthorName}.png`} width={100} alt={author}></Image>
+                </div>
+                <div className="d-flex justify-content-start">
+                    <p className="text-muted mb-0 me-2"> por {author} - {date}</p>
                 </div>
             </div>
             <div className="content" dangerouslySetInnerHTML={{ __html: content }} />
