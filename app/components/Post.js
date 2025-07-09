62f910a045bff92a431df25a298c7113091d0756
mewaiver <metiago.work@gmail.com>
2025-07-09 11:04:38 -0300

    Update Post.js

--- app/components/Post.js
+++ app/components/Post.js
@@ -28,6 +28,13 @@ const Post = ({ title, date, content, category, author }) => {
 
     return (
         <>
+            <div className="header-image">
+        <img 
+            src="https://images.unsplash.com/photo-1667124060133-c1074b0124be?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+            alt="Header" 
+            className="img-fluid" 
+        />
+    </div>
             <div className="text-center">
                 <h1 className="display-4">{title}</h1>
                 <div className="d-flex justify-content-start">
